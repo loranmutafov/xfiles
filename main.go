c427a364e2adcc66307ccb485398d9d8c51c69a6
Loran Mutafov <loranmutafov@gmail.com>
2021-04-06 15:49:57 +0300

    Enable CORS

--- main.go
+++ main.go
@@ -7,11 +7,27 @@ import (
 
 func main() {
 	fs := http.FileServer(http.Dir("/xfiles"))
-	http.Handle("/", fs)
+	http.Handle("/", corsMiddleware(fs))
 
 	log.Println("Listening on :3000...")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func corsMiddleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		origin := r.Header.Get("Origin")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "GET,POST")
+
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, Authorization")
+			return
+		} else {
+			h.ServeHTTP(w, r)
+		}
+	})
 }
